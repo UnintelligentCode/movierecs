5c39d1bc23281127a31b5abbd23b4453ec47a818
UnintelligentCode <victoraliharris@gmail.com>
2022-11-14 01:15:21 -0500

    mra01-1: added div initial style & attribute

--- src/App.fs
+++ src/App.fs
@@ -8,7 +8,9 @@ open Fable.Core
 let body = document.getElementById "body" :?> HTMLBodyElement
 // creation of new elements to be added to the body
 let mainDiv = document.createElement "div" :?> HTMLDivElement
-//setting attribute for main div
+//setting attributes & styles for main div
+mainDiv.setAttribute("id", "movieCard")
+mainDiv.style.setProperty("display", "grid")
 let mainHeader = document.createElement "h1"
 
 // elements to the body
