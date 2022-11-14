3bc3fa52fef7864183d975cfeb5567a95680207c
UnintelligentCode <victoraliharris@gmail.com>
2022-11-14 01:09:51 -0500

    mra01: changed extension for styles, ot body element, appended div & h1 to child, & changed htmlUtils name

--- src/App.fs
+++ src/App.fs
@@ -3,3 +3,17 @@ module App
 open Browser
 open Browser.Types
 open Fable.Core
+
+// getting the body element to add everything else
+let body = document.getElementById "body" :?> HTMLBodyElement
+// creation of new elements to be added to the body
+let mainDiv = document.createElement "div" :?> HTMLDivElement
+//setting attribute for main div
+let mainHeader = document.createElement "h1"
+
+// elements to the body
+body.appendChild mainDiv |> ignore
+mainDiv.appendChild mainHeader |> ignore
+
+// creating header text
+mainHeader.textContent <- "TEST"
