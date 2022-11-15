ce536ac38e07c6d773bb7e0a10592976a56fac75
UnintelligentCode <victoraliharris@gmail.com>
2022-11-14 20:32:42 -0500

    mra01-2: inital layout, css properties, & html attribues added

--- src/App.fs
+++ src/App.fs
@@ -6,16 +6,53 @@ open Fable.Core
 
 // getting the body element to add everything else
 let body = document.getElementById "body" :?> HTMLBodyElement
+let bodySize = 512/32
 // creation of new elements to be added to the body
 let mainDiv = document.createElement "div" :?> HTMLDivElement
 //setting attributes & styles for main div
 mainDiv.setAttribute("id", "movieCard")
 mainDiv.style.setProperty("display", "grid")
-let mainHeader = document.createElement "h1"
+mainDiv.style.setProperty("height", "256px")
+mainDiv.style.setProperty("width", "256px")
+//mainDiv.style.setProperty("grid-template-columns", "repeat(4, 128px")
+mainDiv.style.setProperty("grid-template-rows", "auto")
+console.log bodySize
+
+mainDiv.style.setProperty("grid-template-areas", "\"h1 h1 h1 h1\" \"mc mc mc mc\" \"mc mc mc mc\" \"mc mc mc mc\" \"b1 b1 b2 b2\"")
+
+let mainIMG = document.createElement "img" :?> HTMLImageElement
+mainIMG.setAttribute("id", "mainIMG")
+mainIMG.src <- "twk.jpg"
+mainIMG.style.setProperty("grid-area", "mc")
+mainIMG.style.setProperty("width", "256px")
+
+let goodBtn = document.createElement "button" :?> HTMLButtonElement
+goodBtn.setAttribute("id", "goodBtn")
+goodBtn.textContent <- "Like"
+goodBtn.style.backgroundColor <- "green"
+goodBtn.style.setProperty("grid-row", "b2")
+goodBtn.style.width <- "128px"
+
+let badBtn = document.createElement "button" :?> HTMLButtonElement
+badBtn.setAttribute("id", "badBtn")
+badBtn.textContent <- "Dislike"
+badBtn.style.backgroundColor <- "red"
+badBtn.style.setProperty("grid-area", "b1")
+badBtn.style.width <- "128px"
+
+// creation of card header
+let mainHeader = document.createElement "p"
+//setting attributes & props for styles
+mainHeader.setAttribute("id", "mainHeader")
+mainHeader.textContent <- "TEST"
+mainHeader.style.color <- "red"
+mainHeader.style.setProperty("grid-area", "h1")
+mainHeader.style.width <- "256px"
+mainHeader.style.textAlign <- "center"
 
 // elements to the body
 body.appendChild mainDiv |> ignore
 mainDiv.appendChild mainHeader |> ignore
-
-// creating header text
-mainHeader.textContent <- "TEST"
+mainDiv.appendChild mainIMG |> ignore
+mainDiv.appendChild badBtn |> ignore
+mainDiv.appendChild goodBtn |> ignore
